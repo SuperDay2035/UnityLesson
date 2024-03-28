750f6ff14c825571d6d64e4d876997d05a3ff65c
Unity <64528923+SuperDay2035@users.noreply.github.com>
2024-03-28 17:05:09 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.scss"
+
+import Header from "./Component/Header";
+import Home from "./Component/Home";
+
+function App() {
+  return (
+    <div className="App">
+
+
+        <Header></Header>
+        <Home></Home>
+
+
+    </div>
+  );
+}
+
+export default App;
